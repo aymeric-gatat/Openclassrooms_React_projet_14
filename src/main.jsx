764160f7f995fe324b08475570bdd85e22e67ce8
aymeric-gatat <gatataymeric@hotmail.fr>
2025-07-29 17:47:13 +0200

    Last update for perf

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./styles/style.scss";
-import App from "./pages/App";
-import Employee from "./pages/Employee";
+
+const App = lazy(() => import("./pages/App"));
+const Employee = lazy(() => import("./pages/Employee"));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/employee" element={<Employee />} />
-        </Routes>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/employee" element={<Employee />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </Provider>
   </StrictMode>
